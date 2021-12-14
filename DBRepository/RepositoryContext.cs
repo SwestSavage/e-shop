7708ff3bdbebbb1dfe19aee69335e5b8f1db5a67
Ilya Uzkikh <ilyauzkikh@gmail.com>
2021-12-14 14:56:08 +0300

    Added db repo classes

---
+++ DBRepository/RepositoryContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace DBRepository
+{
+    public class RepositoryContext : DbContext
+    {
+        public RepositoryContext(DbContextOptions<RepositoryContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product>
+    }
+}
---
+++ DBRepository/RepositoryContextFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DBRepository
+{
+    public class RepositoryContextFactory : IRepositoryContextFactory
+    {
+        public RepositoryContext CreateDbContext(string conntectionString)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<RepositoryContext>();
+            optionsBuilder.UseSqlServer(conntectionString);
+
+            return new RepositoryContext(optionsBuilder.Options);
+        }
+    }
+}
