da54ed5a9961a0e0653b0256ee989fa55385e466
Ilya Uzkikh <ilyauzkikh@gmail.com>
2021-12-14 15:35:19 +0300

    Added product controller

---
+++ e-shop/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DBRepository;
+using Models;
+
+namespace e_shop.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProductController : Controller
+    {
+        private IProductRepository _productRepository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        [Route("products")]
+        [HttpGet]
+        public async Task<List<Product>> GetProducts()
+        {
+            return await _productRepository.GetProducts();
+        }
+    }
+}
