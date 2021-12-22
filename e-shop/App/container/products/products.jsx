4ab19e0dab328b0485ca543086ac8fcf3a3c6336
Ilya Uzkikh <ilyauzkikh@gmail.com>
2021-12-22 14:57:54 +0300

    Configure app to show products tab

--- e-shop/App/container/products/products.jsx
+++ e-shop/App/container/products/products.jsx
@@ -1,9 +1,47 @@
 ﻿import React from 'react';
+import { connect } from 'react-redux'
+import { getProducts } from './productsActions.jsx';
 
-export default class Products extends React.Component {
+class Products extends React.Component {
+
+    componentDidMount() {
+        this.props.getProducts();
+    }
+
     render() {
+        console.log('props:');
+        for (let key in this.props.products) {
+            console.log('key: ' + key + ', value: ' + this.props.products[key]);
+        }
+        let products = this.props.products.map(item => {
+            return (
+                <div key={item.id} className='product'>
+                    <div className='title'>{item.title}</div>
+                    <div className='image'>{item.image}</div>
+                    <div className='description'>{item.description}</div>
+                    <div className='price'>{item.price}</div>
+                    <button>Buy</button>
+                </div>
+                )
+        })
+
         return (
-            <div>Продукты</div>
+            <div id='products'>{products}</div>
         );
     }
 };
+
+let mapProps = (state) => {
+    return {
+        products: state.data,
+        error: state.error
+    }
+}
+
+let mapDispatch = (dispatch) => {
+    return {
+        getProducts: () => dispatch(getProducts())
+    }
+}
+
+export default connect(mapProps, mapDispatch)(Products);
--- e-shop/App/index.jsx
+++ e-shop/App/index.jsx
@@ -1,8 +1,20 @@
 ﻿import React from 'react'
 import { render } from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
 import App from './container/app.jsx'
+import productsReducer from './container/products/productsReducer.jsx'
+
+function configureStore(initialState) {
+    return createStore(productsReducer, initialState, applyMiddleware(thunk));
+}
+
+const store = configureStore();
 
 render(
-    <App />,
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.getElementById('content')
 )
